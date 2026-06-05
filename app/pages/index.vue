fdccdbc33af550c752d7856721dd9779469d416e
thaislario972 <TON_EMAIL_GITHUB>
2026-06-05 14:36:03 +0200

    fix détail

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template >
-  <section class="w-full max-w-full overflow-x-hidden md:bg-black bg-none">
-    <div class="bg-gradient-to-br w-full overflow-hidden from-turq/30 via-black to-turq/20 sm:from-lavand/15 min-h-screen sm:min-h-[calc(100vh-6rem)] flex justify-center items-center" >
+  <section class="w-full max-w-full overflow-x-hidden bg-black">
+    <div class="relative w-full max-w-full min-h-screen overflow-x-hidden overflow-y-hidden bg-gradient-to-br from-turq/30 via-black to-turq/20 sm:from-lavand/15 flex justify-center items-center" >
       <!-- calque lumières -->
       <div class="absolute inset-0 pointer-events-none max-w-full">
         <span class="light absolute top-18 left-[20%] color-lavand animation-delay-4000"></span>
